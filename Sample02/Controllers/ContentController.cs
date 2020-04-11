ddd0542b211c0809d76aaa56afe237fc8ecbc542
zhangliang <1002081049@qq.com>
2020-04-11 20:30:19 +0800

    注释内容

---
+++ Sample02/Controllers/ContentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+
+
+        public IActionResult Index()
+        {
+           
+            return View(new ContentViewModel { Contents=new List<Content> { contents} });
+        }
+    }
+}
